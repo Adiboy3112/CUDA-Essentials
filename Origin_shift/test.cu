3b499e134c68987fa2cd38d03ccb411be8805573
Adiboy3112 <adiboy_3112@kgpian.iitkgp.ac.in>
2022-07-09 10:17:19 +0530

    Pointer reallocation works

--- Origin_shift/test.cu
+++ Origin_shift/test.cu
@@ -1,21 +1,31 @@
 #include <cufft.h>
 #include <stdio.h>
 
+    cufftReal *a,*b;
+    cufftComplex *c;
+
+
 __global__ void Adderf(cufftReal *a,float b,float c)
 {
     *a=b+c;
     
 }
+void foo(cufftReal *a)
+{
+    float a1=2.0,b1=3.5;
+    Adderf<<<1,1>>>(a,a1,b1);
+}
 int main()
 {
-    cufftReal *a,b;
-    float a1=2.0,b1=3.5;
     if(cudaMalloc((void**)&a,sizeof(cufftReal))==cudaErrorMemoryAllocation)printf("wgharts");
+    b=(cufftReal *)malloc(sizeof(cufftReal));
   
-    Adderf<<<1,1>>>(a,a1,b1);
+    foo(a);
     cudaDeviceSynchronize();
-    cudaMemcpy(&b,a,sizeof(cufftReal),cudaMemcpyDeviceToHost);
+    cudaMemcpy(b,a,sizeof(cufftReal),cudaMemcpyDeviceToHost);
+    c = (cufftComplex *)b;
+    c->y=1.5;
 
-    printf("%f",b);
+    printf("%f",(c->x+c->y));
 
 }
